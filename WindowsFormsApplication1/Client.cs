db106ea1489db98981b2d7d34656cc73bdd14fe6
Vlad06 <vladimirhuppert@hotmail.fr>
2017-03-31 10:07:34 +0200

    maj classe client

--- WindowsFormsApplication1/Client.cs
+++ WindowsFormsApplication1/Client.cs
@@ -44,171 +44,81 @@ namespace WindowsFormsApplication1
 
         public int NumeroClient
         {
-            get
-            {
-                return numeroClient;
-            }
-
-            set
-            {
-                numeroClient = value;
-            }
+            get  { return numeroClient; }
+            set { numeroClient = value; }
         }
 
         public string RaisonSociale
         {
-            get
-            {
-                return raisonSociale;
-            }
-
-            set
-            {
-                raisonSociale = value.Trim().ToUpper();
-            }
+            get {return raisonSociale; }
+            set { raisonSociale = value.Trim().ToUpper(); }
         }
 
         public string TypeClient
         {
-            get
-            {
-                return typeClient;
-            }
-
-            set
-            {
-                typeClient = value;
-            }
+            get {return typeClient; }
+            set { typeClient = value;}
         }
 
         public string ActiviteClient
         {
-            get
-            {
-                return activiteClient;
-            }
-
-            set
-            {
-                activiteClient = value;
-            }
+            get {return activiteClient;}
+            set{ activiteClient = value; }
         }
 
         public string AdresseClient
         {
-            get
-            {
-                return adresseClient;
-            }
-
-            set
-            {
-                adresseClient = value;
-            }
+            get { return adresseClient; }
+            set{ adresseClient = value; }
         }
 
         public string TelephoneClient
         {
-            get
-            {
-                return telephoneClient;
-            }
-
-            set
-            {
-                telephoneClient = value;
-            }
+            get { return telephoneClient;}
+            set { telephoneClient = value; }
         }
 
         public string FaxClient
         {
-            get
-            {
-                return faxClient;
-            }
-
-            set
-            {
-                faxClient = value;
-            }
+            get { return faxClient; }
+            set { faxClient = value;}
         }
 
         public string EmailClient
         {
-            get
-            {
-                return emailClient;
-            }
-
-            set
-            {
-                emailClient = value;
-            }
+            get { return emailClient; }
+            set { emailClient = value; }
         }
 
         public string NatureClient
         {
-            get
-            {
-                return natureClient;
-            }
-
-            set
-            {
-                natureClient = value;
-            }
+            get { return natureClient; }
+            set { natureClient = value; }
         }
 
         public string DomaineClient
         {
-            get
-            {
-                return domaineClient;
-            }
+            get{ return domaineClient; }
 
-            set
-            {
-                domaineClient = value;
-            }
+            set { domaineClient = value; }
         }
 
         public decimal CaClient
         {
-            get
-            {
-                return caClient;
-            }
-
-            set
-            {
-                caClient = value;
-            }
+            get { return caClient; }
+            set { caClient = value; }
         }
 
         public int EffectifClient
         {
-            get
-            {
-                return effectifClient;
-            }
-
-            set
-            {
-                effectifClient = value;
-            }
+            get { return effectifClient; }
+            set { effectifClient = value; }
         }
 
         public string CommentComm
         {
-            get
-            {
-                return commentComm;
-            }
-
-            set
-            {
-                commentComm = value;
-            }
+            get { return commentComm;  }
+            set { commentComm = value; }
         }
     }
 }
